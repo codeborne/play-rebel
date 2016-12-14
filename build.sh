b5293fd566f7f6e3e4660869bbcc30c535b85fa2
Andrei Solntsev <andrei.solntsev@gmail.com>
2016-12-14 14:17:53 +0200

    use JPARepository instead of JPAEnhancer
    it would allow us to disable JPAEnhancer

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ORGANIZATION="play-rebel"
+MODULE="play-rebel"
+VERSION=`grep self conf/dependencies.yml | sed "s/.*$MODULE //"`
+DESTINATION=/var/www/repo/$ORGANIZATION
+TARGET=$DESTINATION/$MODULE-$VERSION.zip
+
+rm -fr dist
+play dependencies --sync || exit $?
+play build-module || exit $?
+
+if [ -d $DESTINATION ]; then
+  if [ -e $TARGET ]; then
+      echo "Not publishing, $MODULE-$VERSION already exists"
+  else
+      cp dist/*.zip $TARGET
+      echo "Package is available at https://repo.codeborne.com/$ORGANIZATION/$MODULE-$VERSION.zip"
+  fi
+fi
