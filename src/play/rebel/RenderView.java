af1dda0db8d19a12b953f4fc9bba004437077e13
Andrei Solntsev <andrei.solntsev@gmail.com>
2017-09-21 22:33:32 +0300

    play-rebel 1.2: rename `RenderView` to `View`: it's shorter, and it's noun

--- src/play/rebel/RenderView.java
+++ src/play/rebel/RenderView.java
@@ -1,102 +1,21 @@
 package play.rebel;
 
-import play.data.validation.Validation;
-import play.exceptions.UnexpectedException;
-import play.libs.MimeTypes;
-import play.mvc.Http;
-import play.mvc.Scope;
-import play.mvc.results.Result;
-import play.templates.Template;
-import play.templates.TemplateLoader;
-
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
-import static java.util.Collections.emptyMap;
-import static play.rebel.Bridge.template;
-
-/**
- * 200 OK with a template rendering
- */
-public class RenderView extends Result {
-
-  private final String templateName;
-  private final Map<String, Object> arguments = new HashMap<>();
-  private String content;
-  private long renderTime;
-
+public class RenderView extends View {
   public RenderView() {
-    this(template());
   }
-  
+
   public RenderView(String templateName) {
-    this(templateName, emptyMap());
+    super(templateName);
   }
 
   public RenderView(String templateName, Map<String, Object> arguments) {
-    this.templateName = templateName;
-    this.arguments.putAll(Scope.RenderArgs.current().data);
-    this.arguments.putAll(arguments);
-    generateAuthenticityToken();
+    super(templateName, arguments);
   }
 
-  private void generateAuthenticityToken() {
-    // it's important to generate authenticityToken in controller, not in `apply` method
-    Scope.Session.current().getAuthenticityToken();
-  }
-
-  @Override
-  public void apply(Http.Request request, Http.Response response) {
-    try {
-      renderView(response);
-    } catch (Exception e) {
-      throw new UnexpectedException(e);
-    }
-  }
-
-  private void renderView(Http.Response response) throws IOException {
-    long start = System.currentTimeMillis();
-    Template template = resolveTemplate();
-
-    Map<String, Object> templateBinding = new HashMap<>();
-    templateBinding.putAll(arguments);
-    templateBinding.put("session", Scope.Session.current());
-    templateBinding.put("request", Http.Request.current());
-    templateBinding.put("flash", Scope.Flash.current());
-    templateBinding.put("params", Scope.Params.current());
-    templateBinding.put("errors", Validation.errors());
-
-    this.content = template.render(templateBinding);
-    this.renderTime = System.currentTimeMillis() - start;
-    String contentType = MimeTypes.getContentType(template.name, "text/plain");
-    response.out.write(content.getBytes(getEncoding()));
-    setContentTypeIfNotSet(response, contentType);
-  }
-
-  private Template resolveTemplate() {
-    return TemplateLoader.load(template(templateName));
-  }
-  
-  public String getName() {
-    return templateName;
-  }
-
-  public String getContent() {
-    return content;
-  }
-
-  public Map<String, Object> getArguments() {
-    return arguments;
-  }
-
-  public long getRenderTime() {
-    return renderTime;
-  }
-
-  public RenderView with(String name, Object value) {
-    arguments.put(name, value);
-    return this;
+  @Override public RenderView with(String name, Object value) {
+    return (RenderView) super.with(name, value);
   }
 }
 
