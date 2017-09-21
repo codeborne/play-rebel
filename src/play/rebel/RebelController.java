3b94a181d06925eb4c25422e7fc7cce446bd7e1e
Andrei Solntsev <andrei.solntsev@gmail.com>
2017-09-21 22:26:21 +0300

    play-rebel 1.2: remove or deprecated many of static methods in RebelController
    (we recommend to use non-static methods)

--- src/play/rebel/RebelController.java
+++ src/play/rebel/RebelController.java
@@ -1,68 +1,58 @@
 package play.rebel;
 
-import com.thoughtworks.xstream.XStream;
-import org.w3c.dom.Document;
-import play.Logger;
 import play.classloading.enhancers.ControllersEnhancer.ControllerSupport;
 import play.data.validation.Validation;
-import play.exceptions.UnexpectedException;
-import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.PlayController;
 import play.mvc.Scope;
 import play.mvc.results.*;
-import play.mvc.results.Error;
 
-import java.io.File;
 import java.io.InputStream;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Map;
-
-import static java.util.Collections.emptyMap;
 
 /**
  * A superclass for all controllers that are intended to work without play enhancers.
  */
 public class RebelController implements PlayController, ControllerSupport {
 
-  protected Http.Request request = request();
+  protected Http.Request request = Http.Request.current();
 
+  @Deprecated
   protected static Http.Request request() {
     return Http.Request.current();
   }
   
-  protected Http.Response response = response();
-  
-  protected static Http.Response response() {
-    return Http.Response.current();
-  }
+  protected Http.Response response = Http.Response.current();
 
-  protected Scope.Session session = session();
-  
+  protected Scope.Session session = Scope.Session.current();
+
+  @Deprecated
   protected static Scope.Session session() {
     return Scope.Session.current();
   }
 
-  protected Scope.Flash flash = flash();
-  
+  protected Scope.Flash flash = Scope.Flash.current();
+
+  @Deprecated
   protected static Scope.Flash flash() {
     return Scope.Flash.current();
   }
 
+  @Deprecated
   protected static void flash(String key, Object value) {
     Scope.Flash.current().put(key, value);
   }
 
-  protected Scope.Params params = params();
-  
+  protected Scope.Params params = Scope.Params.current();
+
+  @Deprecated
   protected static Scope.Params params() {
     return Scope.Params.current();
   }
   
-  protected Scope.RenderArgs renderArgs = renderArgs();
-  
+  protected Scope.RenderArgs renderArgs = Scope.RenderArgs.current();
+
+  @Deprecated
   protected static Scope.RenderArgs renderArgs() {
     return Scope.RenderArgs.current();
   }
@@ -78,130 +68,24 @@ public class RebelController implements PlayController, ControllerSupport {
     }
   }
 
-  protected void render() {
-    renderTemplate(template(), emptyMap());
-  }
-  
-  protected static void render(String templateName) {
-    renderTemplate(templateName, emptyMap());
-  }
-  
-  protected void renderTemplate(Map<String, Object> args) {
-    renderTemplate(template(), args);
-  }
-  
-  protected static void renderTemplate(String templateName, Map<String, Object> args) {
-    try {
-      Method method = Controller.class.getDeclaredMethod("renderTemplate", String.class, Map.class);
-      method.setAccessible(true);
-      method.invoke(null, templateName, args);
-    }
-    catch (RuntimeException e) {
-      throw e;
-    }
-    catch (InvocationTargetException e) {
-      if (e.getTargetException() instanceof RuntimeException) {
-        throw (RuntimeException) e.getTargetException();
-      }
-      else {
-        throw new UnexpectedException(e.getTargetException());
-      }
-    }
-    catch (Exception e) {
-      throw new UnexpectedException(e);
-    }
-  }
-  
-  protected String template() {
-    return Bridge.template();
-  }
-  
-  protected String template(String templateName) {
-    return Bridge.template(templateName);
-  }
-  
-  protected static void redirect(String url) {
-    throw new Redirect(url);
-  }
-  
-  protected static void redirect(String url, boolean permanent) {
-    throw new Redirect(url, permanent);
-  }
-  
+  @Deprecated
   protected static void renderJSON(String jsonString) {
     throw new RenderJson(jsonString);
   }
 
+  @Deprecated
   protected static void renderJSON(Object o) {
     throw new RenderJson(o);
   }
 
-  protected static void renderText(CharSequence pattern, Object... args) {
-    throw new RenderText(pattern == null ? "" : String.format(pattern.toString(), args));
-  }
-
+  @Deprecated
   protected static void renderText(Object text) {
     throw new RenderText(text == null ? "" : text.toString());
   }
 
-  protected static void renderHtml(Object html) {
-    throw new RenderHtml(html == null ? "" : html.toString());
-  }
-
-  protected static void renderXml(String xml) {
-    throw new RenderXml(xml);
-  }
-
-  protected static void renderXml(Document xml) {
-    throw new RenderXml(xml);
-  }
-
-  protected static void renderXml(Object o) {
-    throw new RenderXml(o);
-  }
-
-  protected static void renderXml(Object o, XStream xstream) {
-    throw new RenderXml(o, xstream);
-  }
-
-  protected static void renderBinary(InputStream is) {
-    throw new RenderBinary(is, null, true);
-  }
-
-  protected static void renderBinary(InputStream is, long length) {
-    throw new RenderBinary(is, null, length, true);
-  }
-
+  @Deprecated
   protected static void renderBinary(InputStream is, String name) {
     throw new RenderBinary(is, name, false);
-  }
-  
-  protected static void renderBinary(InputStream is, String name, long length) {
-    throw new RenderBinary(is, name, length, false);
-  }
-
-  protected static void renderBinary(InputStream is, String name, boolean inline) {
-    throw new RenderBinary(is, name, inline);
-  }
-
-  protected static void renderBinary(InputStream is, String name, long length, boolean inline) {
-    throw new RenderBinary(is, name, length, inline);
-  }
-
-  protected static void renderBinary(InputStream is, String name, String contentType, boolean inline) {
-    throw new RenderBinary(is, name, contentType, inline);
-  }
-
-  protected static void renderBinary(InputStream is, String name, long length, String contentType, boolean inline) {
-    throw new RenderBinary(is, name, length, contentType, inline);
-  }
-
-  protected static void renderBinary(File file) {
-    throw new RenderBinary(file);
-  }
-  
-  protected static void renderBinary(File file, String name) {
-    throw new RenderBinary(file, name);
   }
 
   protected static void forbidden() {
@@ -212,43 +96,40 @@ public class RebelController implements PlayController, ControllerSupport {
     throw new Forbidden(reason);
   }
 
+  @Deprecated
   protected static void notFound() {
     notFound("");
   }
 
+  @Deprecated
   protected static void notFound(String what) {
     throw new NotFound(what);
   }
 
-  protected static void notFoundIfNull(Object o) {
-    if (o == null) {
-      notFound();
-    }
-  }
-
-  protected static void error() {
-    error("Internal Error");
-  }
-
-  protected static void error(Exception reason) {
-    Logger.error(reason, "error()");
-    throw new Error(reason.toString());
-  }
-
+  @Deprecated
   protected static void error(String reason) {
     throw new play.mvc.results.Error(reason);
   }
 
+  @Deprecated
   protected static void error(int status, String reason) {
-    throw new Error(status, reason);
+    throw new play.mvc.results.Error(status, reason);
   }
 
-  protected static void badRequest(String msg) {
-    throw new BadRequest(msg);
+  protected BadRequest badRequest() {
+    return new BadRequest("Bad request");
   }
 
-  protected static void badRequest() {
-    throw new BadRequest("Bad request");
+  protected View viewResult() {
+    return new View();
+  }
+
+  protected View viewResult(String templateName) {
+    return new View(templateName);
+  }
+
+  protected Forbidden forbiddenResult() {
+    return new Forbidden();
   }
 
   protected static <T extends Annotation> T getActionAnnotation(Class<T> annotationClass) {
